7f32e102c7967966fc75fdae7ffdfa4ff95f9ce7
Dineysd <dineysd@hotmail.com>
2022-01-15 15:32:15 -0300

    teste de exemplo

---
+++ __tests__/integration/session.test.js
@@ -0,0 +1,11 @@
+describe('Authenticate', () => {
+    it('A rota /authenticate deve retornar um token JWT valido',() => {
+
+        const x = 2;
+        const y = 4;
+
+        const sum = x + y;
+
+        expect(sum).toBe(6);
+    });
+});
