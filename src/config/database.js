6ba56fb329c2f3c0c87e2a251abefab4e76d3d77
Dineysd <dineysd@hotmail.com>
2022-01-14 23:37:55 -0300

    configurando banco com postgress

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  host: '127.0.0.1',
+  username: 'postgress',
+  password: '123456',
+  database: 'nodeteste',
+  dialect: 'postgress',
+  operatorAliases: false,
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  }
+}
