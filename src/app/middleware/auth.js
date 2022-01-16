f64408b33d14cc27efa3f4f1a6eb9f683005aca5
Dineysd <dineysd@hotmail.com>
2022-01-16 13:43:54 -0300

    add pasta coverage no gitignore

---
+++ src/app/middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
+
+module.exports = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).json({message: 'Token não fornecido!'});
+    }
+
+    const [, token] = authHeader.split(' ');
+
+    try {
+        const decode = await promisify(jwt.verify)(token, process.env.APP_SECRET);
+        req.userId = decode.id;
+
+        return next();
+    } catch(error){ 
+        return res.status(401).json({message: 'Token invalido!'});
+    }
+
+}
